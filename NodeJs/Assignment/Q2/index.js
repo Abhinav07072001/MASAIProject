fd83d6834f400c3c6d7a9d0430776a2e733cddde
Abhinav Dixit <dixitabhinav788@gmail.com>
2025-09-26 16:18:27 +0530

    nodejs assignment Q2 complete

---
+++ NodeJs/Assignment/Q2/index.js
@@ -0,0 +1,10 @@
+const {factorial}= require('./factorial')
+
+const num=5
+console.log(`Factorial of ${num} is : ${factorial(num)}`);
+
+const num1=7;
+console.log(`Factorial of ${num1} is : ${factorial(num1)}`)
+
+const num2=9;
+console.log(`Factorial of ${num2} is : ${factorial(num2)}`)
